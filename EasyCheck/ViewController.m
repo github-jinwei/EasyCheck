0e623082880d4bbe467ccb24e713341b9339f5cc
github-jinwei <winp21@163.com>
2016-04-22 06:17:04 +0800

    Initial Commit

---
+++ EasyCheck/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EasyCheck
+//
+//  Created by JinWei on 16/4/22.
+//  Copyright © 2016年 Weijin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ EasyCheck/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EasyCheck
+//
+//  Created by JinWei on 16/4/22.
+//  Copyright © 2016年 Weijin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
